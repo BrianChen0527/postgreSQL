26651b929459297cc92722ec359ab3c76aa552ab
BrianChen0527 <brianc.boston@gmail.com>
2022-06-20 14:41:34 +0800

    added product database

---
+++ ASUS_SQL/Item.sql
@@ -0,0 +1,28 @@
+create table Item (
+	SaleID VARCHAR(50) NOT NULL,
+	ItemName VARCHAR(50) NOT NULL,
+	ItemID VARCHAR(50) NOT NULL PRIMARY KEY,
+	amount NUMERIC(19, 0) NOT NULL,
+	price NUMERIC(19, 0) NOT NULL
+);
+
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('6de1e4d2-3385-4b65-aefe-ce60b84e8e4b', 'Pontiac', '46-4279483', 7, 1331);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('2c85cc3f-a0a8-4e3a-ade3-4eea16330842', 'Lexus', '25-0124271', 4, 7048);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('89d792b4-86d6-464d-9e9f-7d2654976c4b', 'Lexus', '45-5792758', 4, 7700);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('811119ed-0520-48f5-bdb2-434fe1901617', 'Cadillac', '97-0843011', 5, 262);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('db06fab1-410f-41e3-8dde-8f40da6f87ca', 'Honda', '76-9578022', 10, 2185);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('f55d3c57-d2a5-4b4d-b41c-3df2258dddba', 'Chevrolet', '11-8964528', 7, 2461);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('f0416339-7b30-4393-9035-cfdc4737ae2d', 'Ford', '77-8024939', 1, 9622);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('dcf5bb87-ac70-4b5a-9149-8f26b5f0c69b', 'Lincoln', '25-0791366', 10, 9831);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('586dc941-126d-4467-92e7-0e7dbc11941c', 'Lotus', '59-4028944', 8, 9361);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('f94ab5a8-924c-414c-b36a-6a40320877f5', 'Mercedes-Benz', '59-1337177', 7, 8091);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('34bff7ca-9f50-407b-8db5-9d49b46fd70d', 'Chevrolet', '12-1731697', 7, 4411);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('b090763f-deb1-4722-9fce-7908a5dec986', 'Acura', '30-3640513', 5, 8708);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('3ae92dad-b8a8-48e8-8fb5-976dbd7d3cab', 'Subaru', '13-2971505', 10, 5380);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('dad6bd0e-fb31-4e93-bbc6-c571212df029', 'Dodge', '21-1309937', 9, 3946);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('eb7bbcaf-ac86-4914-80c1-63d10745794a', 'Mitsubishi', '83-6002962', 9, 5126);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('9a67e73b-f015-401f-b78d-19c18d8a7e7e', 'Kia', '85-2627821', 1, 8081);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('fdd31068-f100-4c54-8c8d-e8f52878ecd3', 'GMC', '78-2811240', 2, 3543);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('64f8fe4b-5954-423e-bf3a-dd8433a424cf', 'Subaru', '70-6230674', 7, 8729);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('43e5f77f-ddec-4dfa-96ba-b4d830d8bbf0', 'Lexus', '73-5435692', 10, 4477);
+insert into Item (SaleID, ItemName, ItemID, amount, price) values ('d2c1d380-390c-4b47-92f9-21cefa056711', 'Chevrolet', '34-3526677', 10, 5160);
