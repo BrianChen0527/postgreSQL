968ea2ebc8d29a9081a2bd8254a4adb28ddf5aab
BrianChen0527 <brianc.boston@gmail.com>
2022-06-20 14:42:18 +0800

    added sales database

---
+++ ASUS_SQL/Sale.sql
@@ -0,0 +1,27 @@
+create table Sale (
+	SaleID VARCHAR(50) NOT NULL,
+	SaleDate VARCHAR(50) NOT NULL,
+	employeeID VARCHAR(50) NOT NULL,
+	customerID VARCHAR(50) NOT NULL
+);
+
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('6de1e4d2-3385-4b65-aefe-ce60b84e8e4b', '5/18/2022', 'Udell', 'Brian');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('2c85cc3f-a0a8-4e3a-ade3-4eea16330842', '7/21/2021', 'Tani', 'Alice');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('89d792b4-86d6-464d-9e9f-7d2654976c4b', '6/17/2022', 'Udell', 'Brian');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('811119ed-0520-48f5-bdb2-434fe1901617', '11/5/2021', 'Tani', 'Corissa');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('db06fab1-410f-41e3-8dde-8f40da6f87ca', '2/19/2022', 'Tani', 'Geoffrey');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('f55d3c57-d2a5-4b4d-b41c-3df2258dddba', '10/24/2021', 'Marice', 'Idalina');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('f0416339-7b30-4393-9035-cfdc4737ae2d', '1/27/2022', 'Udell', 'Alice');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('dcf5bb87-ac70-4b5a-9149-8f26b5f0c69b', '4/17/2022', 'Marice', 'Corissa');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('586dc941-126d-4467-92e7-0e7dbc11941c', '1/26/2022', 'Marice', 'Idalina');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('f94ab5a8-924c-414c-b36a-6a40320877f5', '11/13/2021', 'Udell', 'Idalina');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('34bff7ca-9f50-407b-8db5-9d49b46fd70d', '3/12/2022', 'Tani', 'Alice');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('b090763f-deb1-4722-9fce-7908a5dec986', '4/28/2022', 'Udell', 'Geoffrey');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('3ae92dad-b8a8-48e8-8fb5-976dbd7d3cab', '5/21/2022', 'Marice', 'Corissa');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('dad6bd0e-fb31-4e93-bbc6-c571212df029', '4/21/2022', 'Marice', 'Idalina');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('eb7bbcaf-ac86-4914-80c1-63d10745794a', '2/2/2022', 'Tani', 'Corissa');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('9a67e73b-f015-401f-b78d-19c18d8a7e7e', '7/19/2021', 'Udell', 'Idalina');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('fdd31068-f100-4c54-8c8d-e8f52878ecd3', '4/11/2022', 'Marice', 'Geoffrey');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('64f8fe4b-5954-423e-bf3a-dd8433a424cf', '12/14/2021', 'Marice', 'Alice');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('43e5f77f-ddec-4dfa-96ba-b4d830d8bbf0', '7/14/2021', 'Udell', 'Geoffrey');
+insert into Sale (SaleID, SaleDate, employeeID, customerID) values ('d2c1d380-390c-4b47-92f9-21cefa056711', '12/4/2021', 'Tani', 'Brian');
